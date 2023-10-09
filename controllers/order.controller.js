36552627dac2a247e6fab497855c0eccc8162b33
Darwin Labiste <81823285+Darwinsuuu@users.noreply.github.com>
2023-10-09 14:30:21 +0800

    bug fix and for deployment

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -77,14 +77,33 @@ async function updateOrderTransaction(req, res) {
 
         console.log("==================================")
         await models.onlineTransaction.update(data, { where: { id: req.body.id } });
-        console.log("==================================")
 
-        const subContent = data.status == 'Completed' ? 'You order is on processed and will be shipped to you. <br>Thank you for shopping!' : `Unfortunately, your order was declined due to: "${data.remarks}". <br>    Sorry for the inconvenience and thank you!`
+        console.log("==================================")
+        const custId = await models.onlineTransaction.findAll({where: {id: req.body.id}});
+
+        console.log(`custId ${custId[0].custId}`)
+        console.log("==================================")
+        const productLists = await models.sequelize.query(`SELECT P.id, P.product FROM onlinesales OS INNER JOIN products P ON OS.prodId = P.id WHERE OLTransID=${req.body.id}`);
+
+        let productReview = "";
+
+        let link = "https://sjrenewableenergy.com/product/review";
+
+        await productLists[0].forEach(element => {
+            productReview += `<br><a href="${link}/${element.id}/${custId[0].custId}">Click here to add review to <b>${element.product}</b></a>`;
+        });
+
+
+        const subContent = data.status == 'Completed' ? 'You order is processed and will be shipped to you. <br>Thank you for shopping!' : `Unfortunately, your order was declined due to: "${data.remarks}".<br>Sorry for the inconvenience and thank you!`
+
+
 
         const message = `<div style="width: 100%; max-width: 420px; color: #000; padding: 50px 30px; border: 1px solid #3f51b5; border-radius: 4px; font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; margin: 50px auto; font-size: 18px">
                             <h1 style="margin: 0 0 5px; color: #3f51b5 !important">SJ Renewable Energy</h1>
                             <p style="margin: 0 0 15px;">Hi,</p>
                             <p style="margin: 0 0 15px;">${subContent}</p>
+                            ${productLists.length > 0 ? productReview : ''}
+                            <br>
                             <p style="margin: 0;"><b>Note:</b> Do not reply to this email address. Thank you!</p>
                         </div>`;
 
